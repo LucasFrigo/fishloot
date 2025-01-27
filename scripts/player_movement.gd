3b6aace2449a45ff6245b4e0a55be8fceee414d6
luke <luke@insengard>
2025-01-27 18:45:37 -0300

    organized file structure and started player movement

---
+++ scripts/player_movement.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+
+@export var player_move_speed = 5.0;
+@export var player_gravity = 1.0;
+
+var player_target = Vector3.ZERO
+
+func _ready() -> void:
+	pass 
+
+
+func _process(delta: float) -> void:
+	pass
