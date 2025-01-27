b97f9e43556118853ac175e7e35c19cbc9c0acf9
luke <luke@insengard>
2025-01-27 20:17:26 -0300

    added ray cast logic to track player movement based on mouse input

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,14 +1,38 @@
 extends Node3D
 
-
-@export var player_move_speed = 5.0;
-@export var player_gravity = 1.0;
-
-var player_target = Vector3.ZERO
+@onready var player_nav_agent := $NavigationAgent3D
 
 func _ready() -> void:
-	pass 
+    pass 
 
 
 func _process(delta: float) -> void:
-	pass
+    pass
+
+func _input(event):
+    if Input.is_action_just_pressed("left_mouse"):
+        var player_cam := $Camera3D 
+        var mouse_pos = get_viewport().get_mouse_position()
+        
+        # Ray properties
+        var ray_length = 1000
+        var ray_origin = player_cam.project_ray_origin(mouse_pos)
+        var ray_direction = player_cam.project_ray_normal(mouse_pos)
+        var ray_destiny = ray_origin + ray_direction * ray_length
+        
+        var space = get_world_3d().direct_space_state
+        var ray_query = PhysicsRayQueryParameters3D.new()
+        
+        # Configure ray query
+        ray_query.from = ray_origin
+        ray_query.to = ray_destiny
+        ray_query.collide_with_areas = true  
+        ray_query.collide_with_bodies = true 
+        # Set a different collision layer
+        # ray_query.collision_mask = 1
+
+        var ray_result = space.intersect_ray(ray_query)
+
+        if ray_result:
+            print("Hit: ", ray_result.position)
+            player_nav_agent.target_position = ray_result.position
